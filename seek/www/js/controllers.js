46b5ff35e4cc91d3e8de43bff7abe1acbf76cd9f
MyLap\SupulD <supuldulanka@gmail.com>
2016-11-03 19:57:15 +0530

    Search bar added!

--- seek/www/js/controllers.js
+++ seek/www/js/controllers.js
@@ -175,4 +175,28 @@ angular.module('starter.controllers', [])
 
 })
 
+
+.controller('MainCtrl', function($scope, $ionicPopup) {
+$scope.showPopup = function() {
+$scope.data = {};
+$ionicPopup.show({
+template: '<input type="username">',
+title: 'Enter Username',
+subTitle: 'Please Enter Username',
+scope: $scope,
+buttons: [
+{ text: 'Cancel' },
+{
+text: '<b>Save</b>',
+type: 'button-positive',
+onTap: function(e) {
+// add your action
+}
+}
+]
+});
+}
+});
+
+
 ;
