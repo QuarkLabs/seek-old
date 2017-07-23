229d56752298f61e1c96d0ee4023b13d3ade064c
apramodya <pramodyaabeysinghe@gmail.com>
2017-07-23 10:51:37 +0530

    add tags

--- seek/www/js/controllers.js
+++ seek/www/js/controllers.js
@@ -9,13 +9,17 @@ angular.module('app.controllers', [])
 })
 
 
-.controller('searchCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-    // You can include any angular dependencies as parameters for this function
-    // TIP: Access Route Parameters for your page via $stateParams.parameterName
-    function ($scope, $stateParams) {
+    .controller('searchCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+        // You can include any angular dependencies as parameters for this function
+        // TIP: Access Route Parameters for your page via $stateParams.parameterName
+        function ($scope, $stateParams) {
 
 
-    }])
+
+        $scope.searchTag = [{field:"Machine Learning"},{field:"AI"}];
+
+
+        }])
 
     .controller('mapCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
         // You can include any angular dependencies as parameters for this function
